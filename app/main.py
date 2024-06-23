35f64482e84f619c492a3a004a583d8046963ecc
ashishkhanagwal20 <ashishstudy20@gmail.com>
2024-06-23 21:16:16 +0530

    final

--- app/main.py
+++ app/main.py
@@ -23,6 +23,6 @@ app.include_router(auth.router)
 app.include_router(vote.router)
 
 
-@app.get("/", methods=["GET", "HEAD"])
+@app.get("/")
 def root():
     return {"message": "Welcome to my API!!"}
